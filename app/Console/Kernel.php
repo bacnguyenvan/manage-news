66c1f1e9daf1a45286f82b93140df0685230556e
Ti Nguyen <tikmtute@gmail.com>
2020-10-28 09:11:09 +0700

    add

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('command:WisebookAccess')
-                 ->dailyAt('11:08')
+                 ->dailyAt('11:14')
                  ->withoutOverlapping();
     }
 
